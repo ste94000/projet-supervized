40e792f3d460941db6dcedaf001c573589723324
Etienne LEMAITRE <etilemko@free.fr>
2025-04-16 11:10:50 +0200

    s

--- config.py
+++ config.py
@@ -1,9 +1,17 @@
 cluster_labels = {
-    "Légendaire": 0,
-    "Visiteur Express": 7,
-    "Contributeur": 3,
-    "Fantôme": 2,
-    "Fidèle Silencieux": 5
+    "Super Utilisateur": 1,
+    "Visiteur Régulier": 8,
+    "Décrocheur Curieux": 7,
+    "Nouveau ou Unique": 3,
+    "Explorateur Léger": 11,
+    "Fidèle Modéré": 6,
+    "Actif Silencieux": 10,
+    "Fantôme Passif": 9,
+    "Rebond Unique": 2,
+    "Fidèle Silencieux": 5,
+    "Éclaireur": 4,
+    "Visiteur Express": 0
 }
+
 
 level_labels = ["Faible", "Moyen", "Fort", "Très fort"]
