53d9dea1b77d8a20ae8c68b91b532eb8fd672b76
Etienne LEMAITRE <etilemko@free.fr>
2025-04-17 19:47:55 +0200

    new

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 cluster_labels = {
-    "Navigateur Occasionnel": 0,             # Sessions faibles, rebond élevé, France, Windows
-    "Utilisateur Historique Extrême": 1,     # 34k sessions, très ancien, activité exceptionnelle
-    "Premier Contact Minimaliste": 2,        # Sessions < 0.5, presque aucune interaction
-    "Visiteur Unique Rapide": 3,             # Visites ultra-courtes et récentes
-    "Explorateur Régulier": 4,               # Sessions normales, navigation modérée
-    "Mobinaute Côte d’Ivoire": 5,            # Peu de commentaires, OS mobile, contexte local
-    "Explorateur Marocain": 6,               # Sessions modérées, Maroc, usage répétitif
-    "Volatile US Desktop": 7,                # Sessions irrégulières, OS desktop, USA
-    "Curieux Mobile Afrique": 8,             # Côte d'Ivoire, faible ancienneté, usage mobile
-    "Endormi Ancien Suisse": 9,              # Très ancien, rebondé, peu actif récemment
-    "Linuxien Régulier Belgique": 10,        # OS Linux, Belgique, usage modéré/stable
-    "Androidien Tunisien Récurrent": 11      # Android, Tunisie, sessions correctes
+    "Navigateur Occasionnel à Rebond Élevé": 0,
+    "Androidien Récurrent et Actif": 1,
+    "Premier Contact Minimaliste": 2,
+    "Explorateur Régulier Modéré": 3,
+    "Visiteur Unique à Passage Éclair": 4,
+    "Utilisateur Historique Extrême": 5,
+    "Explorateur Récurrent Stable": 6,
+    "Utilisateur Volatile Desktop": 7,
+    "Curieux Mobile à Faible Ancienneté": 8,
+    "Ancien Endormi à Fort Rebond": 9,
+    "Linuxien Régulier et Stable": 10,
+    "Mobinaute Léger sur Réseau Local": 11,
+    "Utilisateur Répétitif Confirmé": 12
 }
-
 level_labels = ["Faible", "Moyen", "Fort", "Très fort"]
