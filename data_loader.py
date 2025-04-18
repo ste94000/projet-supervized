f66ef18ac54bba9ec175d78b20101e18ccabaa6f
Etienne LEMAITRE <etilemko@free.fr>
2025-04-18 10:52:07 +0200

    m

--- data_loader.py
+++ data_loader.py
@@ -5,3 +5,8 @@ import streamlit as st
 def load_data():
     df = pd.read_csv("./dataset_final_3.csv").drop(columns=["Unnamed: 0.1","Unnamed: 0"])
     return df
+
+@st.cache_data
+def load_data_engagement():
+    df = pd.read_csv("./data_explorateur_engagement.csv")
+    return df
